d740f5adffae415ca118e02794132aece6eb886f
daniely9 <danielyakubov92@gmail.com>
2018-09-17 14:33:22 -0400

    Add files via upload

---
+++ ws_4413/First/src/First.java
@@ -0,0 +1,46 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet("/First")
+public class First extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException
+	{
+		super.init();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/plain");
+		PrintWriter pw = response.getWriter();
+		pw.println("Hello 4413!");
+		System.out.println("***Log Request received and response was served.");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		this.doGet(request, response);
+	}
+
+}
